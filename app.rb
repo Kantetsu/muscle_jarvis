65d0a6b24a55f6be333b3a111594959abf9b7bcd
kantetsu <shoshikantetsu0809@gmail.com>
2017-06-10 11:45:31 +0900

    [ADD] youtubeのapiを作成。

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'line/bot'
 require "pry"
 require 'rest-client'
+require 'google/api_client'
+require 'trollop'
+
+DEVELOPER_KEY = ENV["YOUTUBE_API_KEY"]
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 module Line
   module Bot
@@ -12,6 +18,63 @@ module Line
         RestClient.post(url, payload, header)
       end
     end
+  end
+end
+
+def get_service
+  client = Google::APIClient.new(
+    :key => DEVELOPER_KEY,
+    :authorization => nil,
+    :application_name => "muscle-jarvis",
+    :application_version => '1.0.0'
+  )
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+  return client, youtube
+end
+
+def youtube_api(word)
+  opts = Trollop::options do
+    opt :q, 'Search term', :type => String, :default => word
+    opt :max_results, 'Max results', :type => :int, :default => 25
+  end
+
+  client, youtube = get_service
+
+  begin
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = client.execute!(
+      :api_method => youtube.search.list,
+      :parameters => {
+        :part => 'snippet',
+        :q => opts[:q],
+        :maxResults => opts[:max_results]
+      }
+    )
+
+    videos = []
+    channels = []
+    playlists = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    search_response.data.items.each do |search_result|
+      case search_result.id.kind
+        when 'youtube#video'
+          videos << "#{search_result.snippet.title} (#{search_result.id.videoId})"
+        when 'youtube#channel'
+          channels << "#{search_result.snippet.title} (#{search_result.id.channelId})"
+        when 'youtube#playlist'
+          playlists << "#{search_result.snippet.title} (#{search_result.id.playlistId})"
+      end
+    end
+
+    puts "Videos:\n", videos, "\n"
+    puts "Channels:\n", channels, "\n"
+    puts "Playlists:\n", playlists, "\n"
+  rescue Google::APIClient::TransmissionError => e
+    puts e.result.body
   end
 end
 
@@ -41,10 +104,7 @@ post '/callback' do
           type: 'text',
           text: event.message['text']
         }
-        if message[:text] == "プロレス最高"
-          message[:text] = "週刊プロレス見たことあんの？"
-        end
-        client.reply_message(event['replyToken'], message)
+        client.reply_message(event['replyToken'], youtube_api(message))
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
